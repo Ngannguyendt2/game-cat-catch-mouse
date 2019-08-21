a7c3c27622424e12c08ef5a403e021be994cd0ed
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-21 10:50:07 +0700

    repair catch mouse with second

--- main.js
+++ main.js
@@ -1,42 +1,53 @@
 let speedcat = Math.floor(Math.random() * 100);
 let cat = new Cat('tom', 15, speedcat);
-let count=0;
+let count = 0;
 let rats = [];
 
 function createRat() {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 5; i++) {
         let speedmouse = Math.floor(Math.random() * 100);
         let weight = Math.floor(Math.random() * 10);
-        let element = new Mouse('Jerry' + i,weight, speedmouse)
+        let element = new Mouse('Jerry' + i, weight, speedmouse)
         rats.push(element);
         console.log(element)
     }
 }
 
 
-function display(mouse) {
+function display(count) {
     document.getElementById('catName').innerHTML = "Full Name Cat: " + cat.nameCat;
     document.getElementById('catWeight').innerHTML = "Cat Weight: " + cat.getWeight() + "kg";
-    document.getElementById('ratName').innerHTML = "Full Name Rat: " + mouse.nameMouse;
-    document.getElementById('ratWeight').innerHTML = "Rat Weight: " + mouse.weightMouse + " kg";
-    document.getElementById('Number of rats captured').innerHTML = "Score:" + count;
+    document.getElementById('Number of rats captured').innerHTML = "Number of rats captured:" + count;
 }
 
 function catchAll() {
+    count = 0;
+    rats=[];
+    cat.setWeight(15);
+    createRat();
+    let i = 0;
+    let c=setInterval(function () {
 
-    for (let i=0;i<rats.length;i++){
-        if(cat.getSpeedCat() > rats[i].getSpeed()){
-            console.log("Catch it!!!");
-            count++;
-            cat.eatMouse(rats[i].getWeight())
-        }else {
-            console.log("Catch fail!");
+            if (cat.getSpeedCat() > rats[i].getSpeed()) {
+                console.log("Catch it!!!");
+                console.log(i);
+                count++;
+                cat.eatMouse(rats[i].getWeight())
+            } else {
+                console.log("Catch fail!");
+            }
+            display(count);
+            i++;
+            if(i>=rats.length){
+                clearInterval(c);
+
         }
-        display(rats[i].getWeight());
-    }
+
+    },100)
+
 
 }
-createRat();
+
 display();
 
 
